67c2c17407666b1f024757bd592798bae354d465
Elise <elise.amblard@curie.fr>
2020-01-09 11:21:28 +0100

    rangement

---
+++ get_data/get_hubness_scores_minkow.R
@@ -0,0 +1,48 @@
+library(Seurat)
+library(philentropy)
+
+matrix <- read.table("GSE99254_NSCLC.TCell.S9055.count.labeled.pca.txt")
+
+# matrix is PCs x cell
+
+# Range of p values for Lp norm
+p.val <- c(0.1,0.5,1,1.5,2,4)
+
+# Range of k values
+k.val <- c(5,10,20,50,100,200)
+
+# Range of PCs nb values
+pc.val <- c(2,5,10,50,100,500,1000,5000,9047)
+
+# Write the function for the kNN graph
+kNN <- function(data,method="minkowski",k,p) {
+  dist.matrix <- philentropy::distance(t(data),method,p)
+  dist.matrix[lower.tri(dist.matrix, diag=T)] <- NA # make the matrix upper triangular
+  k.nn <- t(apply(dist.matrix,1,function(x) order(x,na.last=T)[1:k]))
+  return(k.nn)
+}
+# Get the k-occurences from the kNN graph
+k.occurence <- function(k.nn) {
+  occurence <- rep(0,nrow(k.nn))
+  names(occurence) <- 1:length(occurence)
+  for (i in 1:length(occurence)) {
+    occurence[i] <- sum(k.nn==i)
+  }
+  return(occurence)
+}
+# Get the scores
+get_scores <- function(data,k.val,p.val,pc.val) {
+  for (pc.nb in p.val) {
+    data <- data[1:pc.nb,]
+    for (k in k.val) {
+      minkow <- list()
+        for (p in p.val) {
+          k.nn <- kNN(data=data,k=k.val,p=p)
+          occu <- k.occurence(k.nn=k.nn)
+          minkow[[which(p.val==p)]] <- occu
+        }
+      saveRDS(minkow, file=paste0("kNN_occurence_",k,"_pca",pc.nb,"_minkow.rds")
+    }
+  }
+}
+get_scores(matrix, k.val=k.val, p.val=p.val, pc.val=pc.val)
