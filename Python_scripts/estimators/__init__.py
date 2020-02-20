8e1a6e22f381f08c91b45d9d0d6b551030e5f535
j-bac <jo.bac@live.fr>
2020-02-20 11:45:04 +0100

    update notebooks

---
+++ Python_scripts/estimators/__init__.py
@@ -0,0 +1,6 @@
+from ._FisherS import randsphere, preprocessing, SeparabilityAnalysis, point_inseparability_to_pointID
+from ._DANCo import dancoDimEst as danco_py
+from ._TwoNN import twonn as twonn_py
+from ._ESS import essLocalDimEst as ess_py
+from ._mada import mada as mada_py
+from ._corint import corint as corint_py
