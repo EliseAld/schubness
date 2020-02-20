b9745d038d0e818f066a5d215618b0b74781e6c9
j-bac <jo.bac@live.fr>
2020-02-20 12:07:12 +0100

    update magic, rm ests

--- Python_scripts/estimators/__init__.py
+++
@@ -1,6 +0,0 @@
-from ._FisherS import randsphere, preprocessing, SeparabilityAnalysis, point_inseparability_to_pointID
-from ._DANCo import dancoDimEst as danco_py
-from ._TwoNN import twonn as twonn_py
-from ._ESS import essLocalDimEst as ess_py
-from ._mada import mada as mada_py
-from ._corint import corint as corint_py
