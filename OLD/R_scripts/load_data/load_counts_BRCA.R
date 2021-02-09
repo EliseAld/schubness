edb012ae437571598881a84e90906eb782e0bf5e
EliseAld <40570458+EliseAld@users.noreply.github.com>
2021-02-09 14:48:47 +0100

    Delete OLD directory

--- OLD/R_scripts/load_data/load_counts_BRCA.R
+++
@@ -1,5 +0,0 @@
-data <- read.csv("/Users/elise/Desktop/GitHub/Hubness_sc/bulkRNAseq/brca.csv",header=T, row.names = 1)
-data_pca <- read.table("/Users/elise/Desktop/GitHub/Hubness_sc/bulkRNAseq/brca_pca_readyforhubness.txt")
-#entropy <- read.table("/Users/elise/Desktop/GitHub/Hubness_sc/data/Baron/entropy_lei/baron_scentropy_v1.csv")
-path_neighbors <- "/Users/elise/Desktop/GitHub/Hubness_sc/R_scripts/hub_network/neighbors_BRCA.rds"
-path_neighbors_pca <- "/Users/elise/Desktop/GitHub/Hubness_sc/R_scripts/hub_network/neighbors_pca_BRCA.rds"
--- OLD/R_scripts/load_data/load_counts_duo.R
+++
@@ -1,8 +0,0 @@
-dataset <- c("Koh","KohTCC","Kumar","KumarTCC","SimKumar4easy","SimKumar4hard","SimKumar8hard","Trapnell","TrapnellTCC")
-data <- pblapply(dataset,
-                 function(x) read.csv(paste0("/Users/elise/Desktop/GitHub/Hubness_sc/duo/counts/",x,".csv"), sep=""))
-data_pca <- pblapply(dataset,
-                     function(x) read.csv(paste0("/Users/elise/Desktop/GitHub/Hubness_sc/duo/pca/",x,"_pca_readyforhubness.csv"), sep=""))
-#entropy <- read.table("/Users/elise/Desktop/GitHub/Hubness_sc/data/Baron/entropy_lei/baron_scentropy_v1.csv")
-#path_neighbors <- "/Users/elise/Desktop/GitHub/Hubness_sc/R_scripts/hub_network/neighbors_bulk.rds"
-#path_neighbors_pca <- "/Users/elise/Desktop/GitHub/Hubness_sc/R_scripts/hub_network/neighbors_pca_bulk.rds"
